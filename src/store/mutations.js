7d093dfc3045a95f3dfebf39b786c13f3be457aa
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-12-04 15:58:31 -0600

    Meals by Ingredient done

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,11 +1,11 @@
 export function setSearchedMeals(state, meals) {
-    state.setSearchedMeals = meals || []
+    state.searchedMeals = meals || []
 }
 
 export function setMealsByLetter(state, meals) {
     state.mealsByLetter = meals || []
 }
-    
-export function setMealsByIngredients(state, meals) {
-    state.setMealsByIngredients = meals || []
+
+export function setMealsByIngredient(state, meals) {
+    state.mealsByIngredient = meals || []
 }
